f275bb01d37f71d8f577ee2c3fd1a38762845f9f
Subhav Pradhan <subhav.m.pradhan@vanderbilt.edu>
2016-07-29 20:04:52 +0000

    Update ResourceMonitor to log total network bandwidth instead of rate.

--- Runtime/ResourceMonitor.py
+++ Runtime/ResourceMonitor.py
@@ -6,13 +6,12 @@ import time
 import sys, getopt
 
 def monitor(dmPID, nmPID):
-    global PREV_SENT
-    global PREV_RECV
-
     dmCPU = 0
     nmCPU = 0
     dmMemory = 0
     nmMemory = 0
+    sent = 0
+    recv = 0
     
     dmProcess = psutil.Process(dmPID)
     dmCPU = dmProcess.cpu_percent()
@@ -25,37 +24,17 @@ def monitor(dmPID, nmPID):
     networkBandwidth = psutil.net_io_counters(pernic=True)["eth0"]
     sent = (networkBandwidth.bytes_sent)            #Bytes
     recv = (networkBandwidth.bytes_recv)            #Bytes
-
-    sentRate = 0.0
-    recvRate = 0.0
-
-    if PREV_SENT != 0:
-        sentRate = (sent - PREV_SENT)/float(5.0)      #Bytes/secs
-    
-    if PREV_RECV != 0:
-        recvRate = (recv - PREV_RECV)/float(5.0)      #Bytes/secs
     
     with open("/home/ubuntu/tmpResourceMonitor/ResourceUsage.csv", "ab") as f:
         writer = csv.writer(f)
-        writer.writerow([(dmCPU + nmCPU), (dmMemory + nmMemory), sentRate, recvRate])
+        writer.writerow([(dmCPU + nmCPU), (dmMemory + nmMemory), sent, recv])
         f.close()
-
-    PREV_SENT = sent
-    PREV_RECV = recv
 
 def print_usage():
     print "USAGE:"
     print "ResourceMonitor --dmPID <DeploymentManager PID> --nmPID <NodeMembership PID>"
 
 def main():
-    # Global variables to keep track of previous network usage. This is required
-    # to find rate of network usage as pstil API only provides total usage.
-    global PREV_SENT
-    global PREV_RECV
-
-    PREV_SENT = 0
-    PREV_RECV = 0    
-
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hdn",
                                    ["help", "dmPID=", "nmPID="])
