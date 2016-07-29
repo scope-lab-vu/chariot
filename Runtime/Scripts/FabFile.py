2504395cc5610b8424247829ec753eb4290c9efb
Subhav Pradhan <subhav.m.pradhan@vanderbilt.edu>
2016-07-29 06:21:48 +0000

    Adding script to control Deployment Managers.

--- Runtime/Scripts/FabFile.py
+++ Runtime/Scripts/FabFile.py
@@ -21,5 +21,13 @@ def stop_node_membership():
     run("nohup /home/ubuntu/workspace/chariot/Runtime/Scripts/NodeMembership.sh stop")
 
 @parallel
+def start_deployment_manager(mongoServer):
+    run("nohup /home/ubuntu/workspace/chariot/Runtime/Scripts/DeploymentManager.sh start "+mongoServer)
+
+@parallel
+def stop_deployment_manager():
+    run("nohup /home/ubuntu/workspace/chariot/Runtime/Scripts/DeploymentManager.sh stop")
+
+@parallel
 def install_python_packages():
-    run('sudo apt-get install -y python-dev; sudo apt-get install -y python-pip; sudo pip install kazoo; sudo pip install pymongo; sudo pip install netifaces')
+    run('sudo apt-get install -y python-dev; sudo apt-get install -y python-pip; sudo pip install kazoo; sudo pip install pymongo; sudo pip install netifaces; sudo pip install pyzmq')
