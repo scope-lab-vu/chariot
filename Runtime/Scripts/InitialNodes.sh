dc89a3fd1818931c48d2bb60caea37a23c94fa8e
Subhav Pradhan <subhav.m.pradhan@vanderbilt.edu>
2016-07-29 20:17:50 +0000

    Updated scripts to launch DeploymentManagers with NodeMembership. These scripts are for IoT paper experiments.

--- Runtime/Scripts/InitialNodes.sh
+++ Runtime/Scripts/InitialNodes.sh
@@ -3,13 +3,18 @@
 case "$1" in
     start)
         echo "Starting initial setup: 10 edison, 10 wifi_cam, 1 entry_terminal"
+        echo "Starting NodeMembership on each node"
         fab -H s1,s2,s3,s4,s5,s6,s7,s8,s9,s10 -f FabFile.py start_node_membership:interface=eth0,network=iot,nodeTemplate=edison,monitoringServer=zk
         fab -H s11,s12,s13,s14,s15,s16,s17,s18,s19,s20 -f FabFile.py start_node_membership:interface=eth0,network=iot,nodeTemplate=wifi_cam,monitoringServer=zk
         fab -H s21 -f FabFile.py start_node_membership:interface=eth0,network=iot,nodeTemplate=entry_terminal,monitoringServer=zk
+        
+        echo "Starting DeploymentManager on each node"
+        fab -H s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19,s20,s21 -f FabFile.py start_deployment_manager:mongoServer=mongo
         ;;
     stop)
         echo "Tearing down initial setup"
         fab -H s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19,s20,s21 -f FabFile.py stop_node_membership
+        fab -H s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19,s20,s21 -f FabFile.py stop_deployment_manager
         ;;
     *)
 
--- Runtime/Scripts/NodeUpdateFirst.sh
+++ Runtime/Scripts/NodeUpdateFirst.sh
@@ -3,13 +3,18 @@
 case "$1" in
     start)
         echo "Adding nodes: 10 edison, 10 wifi_cam, 1 entry_terminal"
+        echo "Starting NodeMembership on each node"
         fab -H s22,s23,s24,s25,s26,s27,s28,s29,s30,s31 -f FabFile.py start_node_membership:interface=eth0,network=iot,nodeTemplate=edison,monitoringServer=zk
         fab -H s32,s33,s34,s35,s36,s37,s38,s39,s40,s41 -f FabFile.py start_node_membership:interface=eth0,network=iot,nodeTemplate=wifi_cam,monitoringServer=zk
         fab -H s42 -f FabFile.py start_node_membership:interface=eth0,network=iot,nodeTemplate=entry_terminal,monitoringServer=zk
+        
+        echo "Starting DeploymentManager on each node"
+        fab -H s22,s23,s24,s25,s26,s27,s28,s29,s30,s31,s32,s33,s34,s35,s36,s37,s38,s39,s40,s41,s42 -f FabFile.py start_deployment_manager:mongoServer=mongo
         ;;
     stop)
         echo "Tearing down first update"
         fab -H s22,s23,s24,s25,s26,s27,s28,s29,s30,s31,s32,s33,s34,s35,s36,s37,s38,s39,s40,s41,s42 -f FabFile.py stop_node_membership
+        fab -H s22,s23,s24,s25,s26,s27,s28,s29,s30,s31,s32,s33,s34,s35,s36,s37,s38,s39,s40,s41,s42 -f FabFile.py stop_deployment_manager
         ;;
     *)
 
