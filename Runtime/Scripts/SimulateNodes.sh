92ca0b613444eab0359e5722fc3f97ce6af336c1
Subhav Pradhan <subhavpradhan@gmail.com>
2016-09-14 16:58:27 -0500

    Minor update to use template name in generated node name.

--- Runtime/Scripts/SimulateNodes.sh
+++ Runtime/Scripts/SimulateNodes.sh
@@ -5,7 +5,7 @@ case "$1" in
 		echo "Starting " "$2" " nodes of template $3"
 		for ((i=1; i<=$2; i++))
 		do
-			NodeName="node""$i"
+			NodeName="$3_$i"
 			python ../SimulateNodeActivity.py --nodeName $NodeName --nodeTemplate $3 --port 0 --action "start"
 		done
 		;;
