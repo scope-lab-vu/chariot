303478c3b6fa2ce608916c75719bd6be5d86fae4
Subhav Pradhan <subhavpradhan@gmail.com>
2016-12-08 14:47:16 -0600

    Adding pip setup file for CHARIOT runtime package.

---
+++ Runtime/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="CHARIOTRuntimePackage",
+    version="0.0.1",
+    packages=["chariot_runtime_libs"],
+    scripts=["scripts/DeploymentManager", "scripts/ResilienceEngine", "scripts/NodeMembership", "scripts/NodeMembershipWatcher", "scripts/ResourceMonitor", "scripts/SimulateNodeActivity"],
+    install_requires=["pyzmq==16.0.2", "pymongo==3.4.0", "z3-solver==4.5.1", "kazoo==2.2.1", "netifaces==0.10.5", "psutil==5.0.0"]
+)
