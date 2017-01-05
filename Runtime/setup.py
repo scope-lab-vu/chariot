0750b70d1b9146c4140cd1995256dae692da8e2b
Subhav Pradhan <subhavpradhan@gmail.com>
2017-01-04 21:04:36 -0600

    Added simple config file as an example. Updated setup.py to place init scripts and chariot config file is correct places.

--- Runtime/setup.py
+++ Runtime/setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="CHARIOTRuntime",
+    name="chariot-runtime",
     version="0.0.1",
-    #data_files=[('/tmp', ['test.txt'])],
+    author="visor-vu",
+    description="CHARIOT Runtime Package (Beta)",
+    license="MIT",
+    url="https://github.com/visor-vu/chariot",
+    data_files=[("/etc/chariot/", ["chariot.conf"]), ("/etc/init.d/", ["init_scripts/chariot-dm", "init_scripts/chariot-nm", "init_scripts/chariot-nmw"])],
     packages=["chariot_runtime_libs"],
-    scripts=["scripts/DeploymentManager", "scripts/ManagementEngine", "scripts/NodeMembership", "scripts/NodeMembershipWatcher", "scripts/ResourceMonitor", "scripts/SimulateNodeActivity"],
-    install_requires=["pyzmq==16.0.2", "pymongo==3.4.0", "z3-solver==4.5.1", "kazoo==2.2.1", "netifaces==0.10.5", "psutil==5.0.0"]
+    scripts=["scripts/chariot-dm", "scripts/chariot-me", "scripts/chariot-nm", "scripts/chariot-nmw", "scripts/chariot-rm", "scripts/chariot-sna"],
+    install_requires=["pyzmq==16.0.2", "pymongo==3.4.0", "z3-solver==4.5.1", "kazoo==2.2.1", "netifaces==0.10.5", "psutil==5.0.0"],
+    zip_safe=False
 )
