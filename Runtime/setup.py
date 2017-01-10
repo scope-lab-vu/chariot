45b9ff50934046452c6797623fc0e74f43932c88
Subhav Pradhan <subhavpradhan@gmail.com>
2017-01-09 20:50:07 -0600

    Adding MANIFEST.in file to ensure data_files are copied.

--- Runtime/setup.py
+++ Runtime/setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name="chariot-runtime",
-    version="0.0.1",
+    version="0.0.2",
     author="visor-vu",
+    email="visorvu@gmail.com",
     description="CHARIOT Runtime Package (Beta)",
     license="MIT",
     url="https://github.com/visor-vu/chariot",
